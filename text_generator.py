66dc417fe572ed6c37e1ccc4b724fe9784955f72
Marat Matourin <59852784+Devloppa@users.noreply.github.com>
2022-10-15 19:27:47 +0200

    Break the dataset into bigrams

--- text_generator.py
+++ text_generator.py
@@ -9,13 +9,15 @@ class MyCorpus:
         self.file_str = ""
         self.tokens = []
         self.unique_tokens = None
+        self.bigrams = None
 
         self.get_file_object()
         self.file_to_string()
         self.regexp_tokenize()
         self.get_unique_tokens()
-        self.print_stats()
-        self.interact_with_corpus()
+        self.create_bigrams()
+        self.print_bigrams_stats()
+        self.interact_with_bigrams()
 
     def __enter__(self):
         return self
@@ -23,27 +25,30 @@ class MyCorpus:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close_file()
 
-    def regexp_tokenize(self):
-        self.tokens = regexp_tokenize(self.file_str, r"[^\s]+")
-
     def close_file(self):
         self.file.close()
-
-    def print_stats(self):
-        print("Corpus statistics")
-        print(f"All tokens: {len(self.tokens)}")
-        print(f"Unique tokens: {len(self.unique_tokens)}")
-
-    def file_to_string(self):
-        self.file_str = self.file.read()
-        self.file.close()
-        self.get_file_object()
 
     def get_file_object(self):
         try:
             self.file = open(self.file_name, "r", encoding="utf-8")
         except FileNotFoundError:
             print("FileNotFound Error. Please input an existing file")
+
+    def file_to_string(self):
+        self.file_str = self.file.read()
+        self.file.close()
+        self.get_file_object()
+
+    def regexp_tokenize(self):
+        self.tokens = regexp_tokenize(self.file_str, r"[^\s]+")
+
+    def print_stats(self):
+        print("Corpus statistics")
+        print(f"All tokens: {len(self.tokens)}")
+        print(f"Unique tokens: {len(self.unique_tokens)}")
+
+    def print_bigrams_stats(self):
+        print(f"{len(self.bigrams)}")
 
     def get_unique_tokens(self):
         self.unique_tokens = set(self.tokens)
@@ -65,6 +70,30 @@ class MyCorpus:
         except IndexError:
             print("Index Error. Please input an integer that is in the range of the corpus")
             self.interact_with_corpus()
+
+    def create_bigrams(self):
+        self.bigrams = [self.tokens[i:i+2] for i in range(len(self.tokens) - 1)]
+
+    def print_bigram(self, num):
+        print(f"Head: {self.bigrams[num][0]} Tail: {self.bigrams[num][1]}")
+
+    def interact_with_bigrams(self):
+        try:
+            user_input = input()
+            if user_input == "exit":
+                return
+            user_input = int(user_input)
+            self.print_bigram(user_input)
+            self.interact_with_bigrams()
+        except ValueError:
+            print("Value Error. Please input an integer.")
+            self.interact_with_bigrams()
+        except TypeError:
+            print("Type Error. Please input an integer.")
+            self.interact_with_bigrams()
+        except IndexError:
+            print("Index Error. Please input a value that is not greater than the number of all bigrams.")
+            self.interact_with_bigrams()
 
 
 app = MyCorpus()
